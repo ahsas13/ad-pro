1df61a3ae25b0d9a7018a336d086ccecef5067e8
ahsas13 <ahsas008@gmail.com>
2021-10-11 22:38:40 +0300

    br_2.3_2.4

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,52 @@
 <template>
-  <h1>Прюветь</h1>
+<v-app>
+<v-navigation-drawer app>
+<v-list-item>
+<v-list-item-content>
+<v-list-item-title class="title">
+КИПУ
+</v-list-item-title>
+<v-list-item-subtitle>
+Учебный проект
+</v-list-item-subtitle>
+</v-list-item-content>
+</v-list-item>
+<v-divider></v-divider>
+<v-list dense>
+<v-list-item-group color="primary">
+<v-list-item>
+<v-list-item-icon>
+<v-icon>mdi-lock</v-icon>
+</v-list-item-icon>
+<v-list-item-content>
+<v-list-item-title>Login</v-list-item-title>
+</v-list-item-content>
+</v-list-item>
+</v-list-item-group>
+</v-list>
+</v-navigation-drawer>
+<v-app-bar app dark color="primary">
+<v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
+<v-spacer></v-spacer>
+<v-toolbar-items class="hidden-sm-and-down">
+<v-btn text><v-icon left>mdi-chart-bubble</v-icon>Link One</v-btn>
+<v-btn text>Link Two</v-btn>
+<v-btn text>Link Three</v-btn>
+</v-toolbar-items>
+</v-app-bar>
+ <v-main>
+<router-view></router-view>
+ </v-main>
+</v-app>
 </template>
 
 <script>
 
 export default {
-  
+data() {
+return {
+drawer: false
+		}
+	}
 };
 </script>
