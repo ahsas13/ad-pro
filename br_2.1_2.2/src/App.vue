939f4c9e7469fe619b5bc8c39daa94000bb61928
ahsas13 <ahsas008@gmail.com>
2021-10-11 22:27:15 +0300

    ad-pro

---
+++ br_2.1_2.2/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+  <v-navigation-drawer app></v-navigation-drawer> 
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn  text><v-icon left>mdi-chart-bubble</v-icon>Link One</v-btn>
+       <v-btn  text>Link Two</v-btn> 
+       <v-btn  text>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-main>
+    <router-view></router-view> 
+  </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  
+};
+</script>
