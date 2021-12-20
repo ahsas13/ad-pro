a9e6f3fa32fd6b90f4050a2f6767322ddf6e73b0
ahsas13 <ahsas008@gmail.com>
2021-12-20 15:31:40 +0300

    Firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,14 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
-}).$mount('#app')
+  store,
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBM9eIsWH-690AzrhP7KKxpym8dE0ZtTfo",
+  authDomain: "ad-pro-14816.firebaseapp.com",
+  projectId: "ad-pro-14816",
+  storageBucket: "ad-pro-14816.appspot.com",
+  messagingSenderId: "42083426722",
+  appId: "1:42083426722:web:20324462598e42a1b992fb",
+  measurementId: "G-JPB7L4Z34Y"
+};$mount('#app')
